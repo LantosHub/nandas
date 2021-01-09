a676b83e243ba94ae611e79528e16581c578a7c1
lantos <l.leong1618@gmail.com>
2021-01-09 11:24:51 +0000

    init

--- main.nim
+++ main.nim
@@ -1,8 +1,8 @@
-import mycsv
-import streams
-import strutils
-import times, strutils
-import parsecsv
+import times
+import strformat
+import os
+import ggplotnim
+
 
 template benchmark*(benchmarkName: string, code: untyped) =
   block:
@@ -12,25 +12,5 @@ template benchmark*(benchmarkName: string, code: untyped) =
     let elapsedStr = elapsed.formatFloat(format = ffDecimal, precision = 3)
     echo "CPU Time [", benchmarkName, "] ", elapsedStr, "s"
 
-
-var lexer: mycsv.CSVLexer
-lexer.file_path = "data/test.csv"
-lexer.file_path = "data/fatData.csv"
-
-
-benchmark "csvParse csvParse":
-    var csv: CsvParser
-    var arr = newSeq[seq[string]]()
-    csv.open(lexer.file_path)
-    while csv.readRow():
-      arr.add csv.row
-
-benchmark "csvParse baseVal":
-    lexer.open newFileStream  lexer.file_path
-    var data = lexer.processStream 
-    echo data.len
-
-benchmark "csvParse string":
-    lexer.open newFileStream  lexer.file_path
-    var data = lexer.processStreamAsString 
-    echo data.len
+benchmark "ggplot readCsvTyped":
+  var df = readCsvTyped("./data/fatData2.csv")
--- main.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-import time
-
-startTime= time.time()
-pd.read_csv("data/fatData1.csv")
-print(time.time()- startTime)
